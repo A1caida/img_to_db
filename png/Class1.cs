5ce75d5716999086a542315bab13ae890350a2b8
A1caida <nimmt74@gmail.com>
2021-03-18 16:25:33 +0400

    first commit

---
+++ png/Class1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MySql.Data.MySqlClient;
+using System.Data.Common;
+using System.Data;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace img_to_db
+{
+    class db
+    {
+        MySqlConnection Connection;
+
+        public db(string server, string user, string pass, string database)
+        {
+            MySqlConnectionStringBuilder Connect = new MySqlConnectionStringBuilder
+            {
+                Server = server,
+                UserID = user,
+                Password = pass,
+                Port = 3306,
+                Database = database,
+                CharacterSet = "utf8"
+            };
+            Connection = new MySqlConnection(Connect.ConnectionString);
+        }
+        public DataTable getTableInfoo(string query)
+        {
+            MySqlCommand queryExecute = new MySqlCommand(query, Connection);
+            DataTable ass = new DataTable();
+            Connection.Open();
+            ass.Load(queryExecute.ExecuteReader());
+            Connection.Close();
+            return ass;
+
+        }
+
+        public int up(string flop)
+        {
+            MainWindow imgg = new MainWindow();
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();           
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+            string filename = "";
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                filename = dlg.FileName;
+            }
+
+
+            var userImage = imgg.ImageToByteArray(Image.FromFile(@filename));
+            
+            MySqlCommand command = Connection.CreateCommand();
+            command.CommandText = "INSERT INTO img_kek(name,img,filesize) VALUES(?name, @img, ?filesize)"; 
+            command.Parameters.AddWithValue("@img", userImage);
+            command.Parameters.Add("?filesize", MySqlDbType.Int32).Value = userImage.Length;
+
+            try
+            {
+                Connection.Open();
+                command.ExecuteNonQuery();
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Connection.Close();
+            }
+            return -1;
+
+        }
+
+
+        public System.Drawing.Image upp(int id)
+        {
+            MemoryStream ms;
+            
+            int floppa = 0;
+            MySqlCommand command = Connection.CreateCommand();
+            command.CommandText = "SELECT * FROM img_kek WHERE id = "+ id;
+            //MySqlDataReader myData;
+            byte[] rawData;
+            try
+            {
+                Connection.Open();
+                using (MySqlDataReader myData = command.ExecuteReader())
+                {
+                    myData.Read();
+                    floppa = myData.GetInt32(myData.GetOrdinal("filesize"));
+                    rawData = new byte[floppa];
+                    myData.GetBytes(myData.GetOrdinal("img"), 0, rawData, 0, (Int32)floppa);
+                    ms = new MemoryStream(rawData);
+                    System.Drawing.Image returnImage = System.Drawing.Image.FromStream(ms);
+                    Connection.Close();
+                    return returnImage;
+                }
+               
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            Connection.Close();
+            return null;
+        }
+    }
+}
---
+++ png/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace img_to_db
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        db con = new db("127.0.0.1", "root", "", "a1caida");
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            idimg.ItemsSource = con.getTableInfoo("SELECT id,  name FROM img_kek").AsDataView();
+        }
+
+        public byte[] ImageToByteArray(System.Drawing.Image imageIn)
+        {
+            MemoryStream ms = new MemoryStream();
+            imageIn.Save(ms, ImageFormat.Png);
+            return ms.ToArray();
+        }
+
+        public static System.Drawing.Image ByteArrayToImage(MemoryStream a)
+        {
+           
+            System.Drawing.Image returnImage = System.Drawing.Image.FromStream(a);
+            return returnImage;
+        }
+
+        public void add(object sender, RoutedEventArgs e)
+        {
+            
+            con.up(flop.Text);
+            idimg.ItemsSource = con.getTableInfoo("SELECT id,  name FROM img_kek").AsDataView();
+        }
+        public void view(object sender, RoutedEventArgs e)
+        {
+
+            System.Drawing.Image a = con.upp(Convert.ToInt32(idimg.SelectedValue));
+            var bitmap = new Bitmap(a);
+            IntPtr bmpPt = bitmap.GetHbitmap();
+            BitmapSource bitmapSource =
+             System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                   bmpPt,
+                   IntPtr.Zero,
+                   Int32Rect.Empty,
+                   BitmapSizeOptions.FromEmptyOptions());
+
+            bitmapSource.Freeze();
+            pick.Source = bitmapSource;
+        }
+    }
+}
